cb38e7b80611d5d7f22303270f019c35c607c430
GSkrt <gregor.skrt@gmail.com>
2020-12-31 07:52:40 +0100

    Removing reactive power from measurements.

--- example.py
+++ example.py
@@ -7,19 +7,18 @@ from datetime import datetime
 # import dds8558
 import DDS8558_pymodbus
 
-PS_Spodaj = DDS8558_pymodbus.DDS8558_Modbus_pymodbus(serial_device='/dev/ttyUSB0', modbus_address=0x01, baudrate=9600)
+SmartMeter = DDS8558_pymodbus.DDS8558_Modbus_pymodbus(serial_device='/dev/ttyUSB0', modbus_address=0x01, baudrate=9600)
 
-PS_Spodaj.read_frequency()
+SmartMeter.read_frequency()
 
 try:
     while True:
-        print(datetime.now(),' ', PS_Spodaj.read_active_power(), 'kW ',
-              PS_Spodaj.read_voltage(), 'V ',
-              PS_Spodaj.read_current(), ' A ',
-              PS_Spodaj.read_frequency(), ' Hz ',
-              PS_Spodaj.read_total_active_energy(), ' kWh ',
-              PS_Spodaj.read_total_reactive_energy(), ' kVAr ',
-              'cos(fi)=', PS_Spodaj.read_power_factor()
+        print(datetime.now(),' ', SmartMeter.read_active_power(), 'kW ',
+              SmartMeter.read_voltage(), 'V ',
+              SmartMeter.read_current(), ' A ',
+              SmartMeter.read_frequency(), ' Hz ',
+              SmartMeter.read_total_active_energy(), ' kWh ',
+              'cos(fi)=', SmartMeter.read_power_factor()
               )
         time.sleep(0.1)  # sleep for 1 second and then read data again
 
